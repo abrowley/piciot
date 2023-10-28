a71f6e2854dc3e805a6db6cd6bd5bf405ab6e5db
Alex Rowley <alex.rowley@gmail.com>
2023-10-28 14:25:24 +0100

    added openocd.cfg|

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,9 @@
 int main() {
     setup_default_uart();
     std::cout << "piciot starting" << std::endl;
+    while (true){
+        std::cout << "Alive " << std::endl;
+        sleep_ms(1000);
+    }
     return 0;
 }
