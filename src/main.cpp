e94b01f0861af3f7ed802c26cc217ee81a14e57f
Alex Rowley <alex.rowley@gmail.com>
2023-10-28 15:49:28 +0100

    freertos working|

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <pico/stdlib.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define LED_PIN 15
+
+void vBlinkTask(void*) {
+    for (;;) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(250);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(250);
+    }
+}
 
 int main() {
     setup_default_uart();
     std::cout << "piciot starting" << std::endl;
-    while (true){
-        std::cout << "Alive " << std::endl;
-        sleep_ms(1000);
-    }
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN,GPIO_OUT);
+    xTaskCreate(vBlinkTask, "Blink Task", 128, nullptr, 1, nullptr);
+    vTaskStartScheduler();
     return 0;
 }
