7554fd556173a20340fec5e1695d28477cb0ebc1
Alex Rowley <alex.rowley@gmail.com>
2023-10-29 19:30:38 +0000

    refactor version

--- src/main.cpp
+++ src/main.cpp
@@ -286,7 +286,7 @@ void vMqttTask(void *) {
 
 int main() {
     stdio_init_all();
-    DEBUG_printf("piciot version %s starting\n",GIT_VERSION);
+    DEBUG_printf("piciot version %s starting\n",PICIOT_VERSION);
     xTaskCreate(vMqttTask, "MQTT", 256, nullptr, 1, nullptr);
     xTaskCreate(vBlinkTask, "LED", 128, nullptr, 1, nullptr);
     vTaskStartScheduler();
