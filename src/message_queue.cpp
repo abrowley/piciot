e44e3724eca2c7ca85081a1670bcdd688d5e59e8
Alex Rowley <alex.rowley@gmail.com>
2023-12-10 13:39:30 +0000

    added message queue

---
+++ src/message_queue.cpp
@@ -0,0 +1,17 @@
+//
+// Created by alexr on 09/12/23.
+//
+#include <pico/stdlib.h>
+#include <pico/cyw43_arch.h>
+#include "message_queue.h"
+
+
+MSG_QUEUE_T *message_queue_init() {
+    auto *msg_queue = static_cast<MSG_QUEUE_T *>(calloc(1, sizeof(MSG_QUEUE_T)));
+    if (!msg_queue) {
+        DEBUG_printf("failed to allocate queue\n");
+        return nullptr;
+    }
+    msg_queue->queue = xQueueCreate(10,sizeof (char[100]));
+    return msg_queue;
+}
---
+++ src/message_queue.h
@@ -0,0 +1,18 @@
+//
+// Created by alexr on 09/12/23.
+//
+
+#ifndef PICIOT_MESSAGE_QUEUE_H
+#define PICIOT_MESSAGE_QUEUE_H
+
+#define DEBUG_printf printf
+#include "FreeRTOS.h"
+#include "queue.h"
+
+typedef struct MSG_QUEUE_T_ {
+    QueueHandle_t queue;
+} MSG_QUEUE_T;
+
+MSG_QUEUE_T* message_queue_init();
+
+#endif //PICIOT_MESSAGE_QUEUE_H
